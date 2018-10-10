5c98525fa117cf0be34b39b364337ea593567d3b
DungDuRu <tjsxo21@naver.com>
2018-10-10 23:07:57 +0900

    몽고DB 접속주소 localhost로 변경

--- AccountList.js
+++ AccountList.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://innoboost:teamInno321!@ec2-13-125-248-157.ap-northeast-2.compute.amazonaws.com:27017/admin', { dbName: 'innoboost' });
+mongoose.connect('mongodb://localhost:27017/admin', { dbName: 'innoboost' });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
